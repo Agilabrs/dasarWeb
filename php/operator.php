a7eed8bace6542625666f3a2c9ffb5a12c963b7a
Agilabrs <arbiansyah.agil11@gmail.com>
2024-09-19 07:26:15 +0700

    soal 3.2

---
+++ php/operator.php
@@ -0,0 +1,34 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisabagi = $a % $b;
+$pangkat = $a ** $b;
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Variabel a: {$a} <br>";
+echo "Variabel b: {$b} <br>";
+echo "Penambahan: {$hasilTambah} <br>";
+echo "Pengurangan: {$hasilKurang} <br>";
+echo "Perkalian: {$hasilKali} <br>";
+echo "Pembagian: {$hasilBagi} <br>";
+echo "Sisa Bagi: {$sisabagi} <br>";
+echo "Perpangkatan: {$pangkat} <br>";
+
+echo "Persamaan: {$hasilSama} <br>";
+echo "Pertidaksamaan: {$hasilTidakSama} <br>";
+echo "Kurang dari: {$hasilLebihKecil} <br>";
+echo "Lebih dari: {$hasilLebihBesar} <br>";
+echo "Kurang dari sama: {$hasilLebihKecilSama} <br>";
+echo "Lebih dari sama: {$hasilLebihBesarSama} <br>";
+?>
